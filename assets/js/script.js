ca61e543f331a73ffbb9ffdc8f63ea2d5292d33a
robisonja <jadenrobison8@gmail.com>
2021-02-19 19:20:56 -0700

    psuedocode for html/js

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+//variables
+
+//object to contain questions and answers
+
+//startQuiz function
+
+//loadHighScores function
+
+//answer right/wrong function
+
+//display highscores with list items/highscores array
+
+//save to local storage
+
+//show quiz start page
+
+//clear high scores
+
+//start timer
+
+//reset timer
+
+//clear timer
+
+//show questions
+
+// compare user choice to answer
+
+
+
+//event listeners
+//start quiz
+
+//hide answer
+
+//highscore click
+
+//start over button
+
+//clear highscores button
+
+//player initials submit button
